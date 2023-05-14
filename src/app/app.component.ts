c73a7172f0f75cd4147d24e0379af3a64d04fb50
martapla <martaplanella@hotmail.com>
2023-05-14 19:15:12 +0200

    ngFor,ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Directives';
+  title = 'Users Register';
+  message = "";
+  registered = false;
+  name: string = "";
+  surname: string = "";
+  entries: { name: string }[];  // or: entries: any[];
+
+  constructor() {
+    this.entries = [
+      { name: "Blue flower" },
+      { name: "Yellow flower" },
+      { name: "Orange flower" },
+      { name: "Pink flower" },
+    ];
+  }
+
+  registerUser() {
+    this.registered = true;
+    this.message = "User Registered!";
+    
+  }
+  
 }
+
+
